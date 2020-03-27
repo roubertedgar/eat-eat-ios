b1984e14c65423edb09062a3a0a21d9c2fdd5cf5
Roubert Edgar <roubertedgar@gmail.com>
2020-03-27 12:28:53 -0300

    Adds login button text to button title

--- eateat/Login/LoginViewController.swift
+++ eateat/Login/LoginViewController.swift
@@ -9,6 +9,8 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextImput: UITextField!
     @IBOutlet weak var passwordTextImput: UITextField!
     
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,13 +18,14 @@ class LoginViewController: UIViewController {
     }
     
     private func setupViews(){
-        loginTitleeText.text = getString("loginScreenTitle")
+        loginTitleeText.text = getString("app_name")
         emailLabel.text = getString("emaiNameText")
         passwordLabel.text = getString("passwordText")
         
         emailTextImput.placeholder = getString("loginTextImputHint")
         passwordTextImput.placeholder = getString("passwordTextImputHint")
         
+        loginButton.setTitle(getString("login_button_text"), for: .normal)
     }
     
     private func getString(_ key: String) -> String{
