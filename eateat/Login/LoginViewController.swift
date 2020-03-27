6416394c4278019459f55750ff9ce8212fda0ef0
Roubert Edgar <roubertedgar@gmail.com>
2020-03-27 11:44:12 -0300

    Project internationalization english and portuguese

--- eateat/Login/LoginViewController.swift
+++ eateat/Login/LoginViewController.swift
@@ -2,14 +2,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var loginTitleText: UILabel!
-    @IBOutlet weak var nameText: UILabel!
-    @IBOutlet weak var passwordText: UILabel!
+    @IBOutlet weak var loginTitleeText: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
     
-    @IBOutlet weak var nameInputText: UITextField!
-    @IBOutlet weak var passwordInputText: UITextField!
+    @IBOutlet weak var emailTextImput: UITextField!
+    @IBOutlet weak var passwordTextImput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
     }
+    
+    private func setupViews(){
+        loginTitleeText.text = getString("loginScreenTitle")
+        emailLabel.text = getString("emaiNameText")
+        passwordLabel.text = getString("passwordText")
+        
+        emailTextImput.placeholder = getString("loginTextImputHint")
+        passwordTextImput.placeholder = getString("passwordTextImputHint")
+        
+    }
+    
+    private func getString(_ key: String) -> String{
+        return NSLocalizedString(key, comment: "")
+    }
+    
 }
