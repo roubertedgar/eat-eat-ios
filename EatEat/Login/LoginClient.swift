d7bf22b14110d6d59a11ede800e5e861749b8e88
Roubert Edgar <roubertedgar@gmail.com>
2020-04-21 20:37:15 -0300

    Switch from URLSession to AlamoFire

--- EatEat/Login/LoginClient.swift
+++ EatEat/Login/LoginClient.swift
@@ -1,18 +1,10 @@
-import Foundation
+import Alamofire
 
 class LoginClient {
     
     func doLogin() {
-        let session = URLSession.shared
-        let url = URL(string: "http://localhost:8080/login")!
-        var request = URLRequest(url: url)
-        
-        request.httpMethod = "GET"
-
-        let task = session.dataTask(with: request, completionHandler: { data, response, error in
-            print("chegou a resposta \(data) \(response) \(error)")
-        })
-
-        task.resume()
+        AF.request("http:localhost:8080/login").response { response in
+            debugPrint(response)
+        }
     }
 }
