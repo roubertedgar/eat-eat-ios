68e9c848350c3bd237980effc9b7458c5fadf7c8
Roubert Edgar <roubertedgar@gmail.com>
2020-04-22 01:03:22 -0300

    Creates http manager to wrap alamofire functions

--- EatEat/Login/LoginClient.swift
+++ EatEat/Login/LoginClient.swift
@@ -1,15 +1,15 @@
-import Alamofire
+import Foundation
 
 class LoginClient {
-    
     func doLogin() {
-        AF.request("http://localhost:8080/login", method: .get).responseString { response in
-            switch response.result {
-            case let .success(data):
-                debugPrint(data)
-            case let .failure(error):
-                debugPrint(error)
+        HttpManager(baseUrl: "http//:localhost:8080").get(path: "/login")
+            .responseString { response in
+                switch response.result {
+                case let .success(data):
+                    debugPrint(data)
+                case let .failure(error):
+                    debugPrint(error)
+                }
             }
-        }
     }
 }
