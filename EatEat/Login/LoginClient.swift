18f286d8e5adc7657747522cbdc6a10f6d0aeba0
Roubert Edgar <roubertedgar@gmail.com>
2020-04-22 00:05:20 -0300

    Removes unused user config folders

--- EatEat/Login/LoginClient.swift
+++ EatEat/Login/LoginClient.swift
@@ -3,8 +3,13 @@ import Alamofire
 class LoginClient {
     
     func doLogin() {
-        AF.request("http:localhost:8080/login").response { response in
-            debugPrint(response)
+        AF.request("http://localhost:8080/login", method: .get).responseString { response in
+            switch response.result {
+            case let .success(data):
+                debugPrint(data)
+            case let .failure(error):
+                debugPrint(error)
+            }
         }
     }
 }
